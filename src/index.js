25bde3cbeda4d9b50f532d8092ed39deeda6142d
padoling <hysm920@gmail.com>
2020-06-10 13:30:30 +0900

    Board 컴포넌트에 state 저장하기

--- src/index.js
+++ src/index.js
@@ -4,20 +4,13 @@ import './index.css';
 
 // render <button>
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
       <button 
         className="square" 
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -25,8 +18,25 @@ class Square extends React.Component {
 
 // render 9 squares
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)} />
+    );
   }
 
   render() {
